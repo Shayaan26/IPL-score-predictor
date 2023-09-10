8035fd947add9c9df3e569c25d824773c53789bf
Shayaan26 <122310344+Shayaan26@users.noreply.github.com>
2023-09-10 22:53:27 +0530

    Add files via upload

---
+++ IPL-First-Innings-Score-Prediction-Deployment-master/First Innings Score Prediction - IPL.py
@@ -0,0 +1,94 @@
+# Importing essential libraries
+import pandas as pd
+import pickle
+import matplotlib.pyplot as plt
+# Loading the dataset
+df = pd.read_csv('ipl.csv')
+
+# --- Data Cleaning ---
+
+
+
+
+
+
+
+# Removing unwanted columns
+columns_to_remove = ['mid', 'venue', 'batsman', 'bowler', 'striker', 'non-striker']
+
+
+# Keeping only consistent teams
+
+
+# Removing the first 5 overs data in every match
+df = df[df['overs']>=5.0]
+
+# Converting the column 'date' from string into datetime object
+from datetime import datetime
+df['date'] = df['date'].apply(lambda x: datetime.strptime(x, '%d-%m-%Y'))
+print(df['bat_team'].unique())
+consistent_teams = ['Kolkata Knight Riders', 'Chennai Super Kings', 'Rajasthan Royals',
+                    'Mumbai Indians', 'Kings XI Punjab', 'Royal Challengers Bangalore',
+                    'Delhi Daredevils', 'Sunrisers Hyderabad']
+df = df[(df['bat_team'].isin(consistent_teams)) & (df['bowl_team'].isin(consistent_teams))]
+
+# --- Data Preprocessing ---
+# Converting categorical features using OneHotEncoding method
+
+encoded_df = pd.get_dummies(data=df, columns=['bat_team', 'bowl_team'])
+# 
+
+# Rearranging the columns
+encoded_df = encoded_df[['date', 'bat_team_Chennai Super Kings', 'bat_team_Delhi Daredevils', 'bat_team_Kings XI Punjab',
+              'bat_team_Kolkata Knight Riders', 'bat_team_Mumbai Indians', 'bat_team_Rajasthan Royals',
+              'bat_team_Royal Challengers Bangalore', 'bat_team_Sunrisers Hyderabad',
+              'bowl_team_Chennai Super Kings', 'bowl_team_Delhi Daredevils', 'bowl_team_Kings XI Punjab',
+              'bowl_team_Kolkata Knight Riders', 'bowl_team_Mumbai Indians', 'bowl_team_Rajasthan Royals',
+              'bowl_team_Royal Challengers Bangalore', 'bowl_team_Sunrisers Hyderabad',
+              'overs', 'runs', 'wickets', 'runs_last_5', 'wickets_last_5', 'total']]
+
+print(encoded_df.head(10))
+# Splitting the data into train and test set
+X_train = encoded_df.drop(labels='total', axis=1)[encoded_df['date'].dt.year <= 2016]
+X_test = encoded_df.drop(labels='total', axis=1)[encoded_df['date'].dt.year >= 2017]
+
+y_train = encoded_df[encoded_df['date'].dt.year <= 2016]['total'].values
+y_test = encoded_df[encoded_df['date'].dt.year >= 2017]['total'].values
+
+# Removing the 'date' column
+X_train.drop(labels='date', axis=True, inplace=True)
+X_test.drop(labels='date', axis=True, inplace=True)
+
+# --- Model Building ---
+# Linear Regression Model
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
+
+# Creating a pickle file for the classifier
+filename = 'first-innings-score-lr-model.pkl'
+pickle.dump(regressor, open(filename, 'wb'))
+y_pred = regressor.predict(X_test)
+
+print(y_pred)
+
+# for Accurarcy
+a=list()
+
+
+
+# define the range
+range_min = -10
+range_max = 10
+
+# count the number of predictions within the range
+count_within_range = 0
+for i in range(len(y_test)):
+    if y_pred[i] >= y_test[i] + range_min and y_pred[i] <= y_test[i] + range_max:
+        count_within_range += 1
+
+# calculate the percentage of predictions within the range
+percentage_within_range = (count_within_range / len(y_test)) * 100
+print(f"Percentage of predictions within the range {range_min} to {range_max}: {percentage_within_range}%")
+
+
